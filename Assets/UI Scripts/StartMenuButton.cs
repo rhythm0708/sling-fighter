c71f5445ebe558478ffc884c161b60d63d8486a8
Chang <csuliang@ucdavis.edu>
2024-03-04 00:06:35 -0800

    Revert "Merge pull request #1 from rhythm0708/Menu"
    This reverts commit 9e47a7c3ef634f01fc01e73495e064c1c632ebff, reversing
    changes made to f606b5518762d919f5bb97859cf655f59e355f08.

--- Assets/UI Scripts/StartMenuButton.cs
+++ Assets/UI Scripts/StartMenuButton.cs
@@ -1,30 +1,10 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    private bool collisionDetected = false;
-
-    private void OnCollisionEnter(Collision collision)
+    private void OnMouseUpAsButton()
     {
-        // Check if the colliding object it's the ball
-        if (collision.gameObject.CompareTag("Ball") && !collisionDetected)
-        {
-            // Set collisionDetected to true to prevent multiple scene switches
-            collisionDetected = true;
-
-            // Start the timer coroutine
-            StartCoroutine(SceneSwitchTimer());
-        }
-    }
-
-    private IEnumerator SceneSwitchTimer()
-    {
-        // Wait for 3 seconds then switch scenes
-        yield return new WaitForSeconds(3f);
-
-        // Change the scene after the delay
         SceneManager.LoadScene("ThirdPersonTest");
     }
 }
