9aed7eb9d166b8fbbcd2b892ecf9132965a26ef7
Chang <csuliang@ucdavis.edu>
2024-03-10 12:54:21 -0700

    Pause Menu
    -Implemented some stuff from Pause Menu
    -Not fully functional, need to fix stuff and discuss it with the team.

--- Assets/Scripts/UI Scripts/PauseMenu.cs
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -5,18 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    private bool isPaused;
+    private string previousScene;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Store the name of the initial scene
+        previousScene = SceneManager.GetActiveScene().name;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)) 
         {
-            SceneManager.LoadScene("Pause Menu");
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
         }
+    }
+
+    public void PauseGame()
+    {
+        SceneManager.LoadScene("Pause Menu", LoadSceneMode.Additive);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        // Unload the pause menu scene
+        SceneManager.UnloadSceneAsync("Pause Menu");
+
+        // Load back the previous scene
+        SceneManager.LoadScene(previousScene);
     }
 }
