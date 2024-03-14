e71d79cc2828420f829c16b752f100f108dc7465
rhythm0708 <rhythmfever0708@gmail.com>
2024-03-14 09:54:20 -0700

    Make Dummy Controller script

---
+++ Assets/Scripts/Enemy Scripts/DummyController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyController : MonoBehaviour
+{
+    // Variables.
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float health;
+
+    // Public getters.
+    public float GetMaxHealth { get => maxHealth; }
+    public float GetHealth { get => health; }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
