8c814e4f9aad4ee4a8b84ea95e70bf37f634e26d
Matvey Volkov <mvolkov@ucdavis.edu>
2024-03-15 09:57:39 -0700

    More Waves and Oscillator
    - 14 waves total now
    - Added Oscillator obstacle

---
+++ Assets/Scripts/OscillatorScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscillatorScript : MonoBehaviour
+{
+    [SerializeField] private float sidestepSpeed;
+    [SerializeField] private Vector3 targetPos1;
+    [SerializeField] private Vector3 targetPos2;
+    private Vector3 currTargetPos;
+
+    void Awake()
+    {
+        currTargetPos = targetPos1;
+    }
+    
+    void Update()
+    {
+        // Oscillate.
+        if (transform.position == targetPos1)
+        {
+            currTargetPos = targetPos2;
+        }
+        else if (transform.position == targetPos2)
+        {
+            currTargetPos = targetPos1;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currTargetPos, sidestepSpeed);
+    }
+}
