ea0bbf947091d68fcfa196ea222dc58171d4f949
Chang <csuliang@ucdavis.edu>
2024-03-16 20:57:58 -0700

    VolumeSettings are saved in Slider

--- Assets/Scripts/UI/VolumeSettings.cs
+++ Assets/Scripts/UI/VolumeSettings.cs
@@ -8,18 +8,51 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider masterSlider;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
+
+    private string masterVolumeKey = "MasterVolume";
+    private string musicVolumeKey = "MusicVolume";
+    private string sfxVolumeKey = "SFXVolume";
+
+    private void Start()
+    {
+        // Load previous volume settings
+        masterSlider.value = PlayerPrefs.GetFloat(masterVolumeKey, 0f);
+        musicSlider.value = PlayerPrefs.GetFloat(musicVolumeKey, 0f);
+        sfxSlider.value = PlayerPrefs.GetFloat(sfxVolumeKey, 0f);
+        
+        // Apply loaded volume settings
+        SetMasterVolume();
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+
     public void SetMasterVolume()
     {
-        mainMixer.SetFloat("MasterVol", masterSlider.value);
+        float volume = masterSlider.value;
+        mainMixer.SetFloat("MasterVol", volume);
+        PlayerPrefs.SetFloat(masterVolumeKey, volume);
+        
+        // Save the value immediately
+        PlayerPrefs.Save(); 
     }
 
     public void SetMusicVolume()
     {
-        mainMixer.SetFloat("MusicVol", musicSlider.value);
+        float volume = musicSlider.value;
+        mainMixer.SetFloat("MusicVol", volume);
+        PlayerPrefs.SetFloat(musicVolumeKey, volume);
+
+        // Save the value immediately
+        PlayerPrefs.Save(); 
     }
 
     public void SetSFXVolume()
     {
-        mainMixer.SetFloat("SFXVol", sfxSlider.value);
+        float volume = sfxSlider.value;
+        mainMixer.SetFloat("SFXVol", volume);
+        PlayerPrefs.SetFloat(sfxVolumeKey, volume);
+
+        // Save the value immediately
+        PlayerPrefs.Save();
     }
 }
