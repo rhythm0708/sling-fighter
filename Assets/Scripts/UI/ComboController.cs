76db6de272e16ee7b269b862e21fde8fed67d109
rhythm0708 <rhythmfever0708@gmail.com>
2024-03-21 11:48:47 -0700

    Combo Count Animation

--- Assets/Scripts/UI/ComboController.cs
+++ Assets/Scripts/UI/ComboController.cs
@@ -6,13 +6,24 @@ using TMPro;
 public class ComboController : MonoBehaviour
 {
     [SerializeField] TMP_Text comboText;
+    [SerializeField] Color originalTextColor;
     private int prevCombo;
     private float timeStamp;
     [SerializeField] float resetTime;
 
+    // Pop out animation.
+    [SerializeField] float animationTime;
+    [SerializeField] float animationLength;
+    [SerializeField] float minFontSize;
+    [SerializeField] float maxFontSize;
+
+
     void Start()
     {
         timeStamp = 0.0f;
+
+        // Subscribe on hit.
+        GameManager.Instance.dummy.SubscribeOnHitByPlayer(AnimateNumber);
     }
 
     void Update()
@@ -34,6 +45,9 @@ public class ComboController : MonoBehaviour
             {
                 prevCombo = comboCount;
                 timeStamp = 0;
+
+                // Reset
+                StartCoroutine(ShrinkNumber());
             }
             else
             {
@@ -43,9 +57,52 @@ public class ComboController : MonoBehaviour
         }
     }
 
-    private IEnumerator PopOutText()
+    private void AnimateNumber()
     {
+        var t = animationTime / animationLength;
+        comboText.fontSize = minFontSize;
+        comboText.color = originalTextColor;
 
+        while (t<1)
+        {
+            Debug.Log("cycles");
+            // Pop out.
+            var textFont = Mathf.Lerp(minFontSize, maxFontSize, t);
+            comboText.fontSize = textFont;
+
+            // Increase opacity.
+            var opacity = Mathf.Lerp(originalTextColor.a, 255f, t);
+            comboText.color = new Color(originalTextColor.r, originalTextColor.g, originalTextColor.b, opacity);
+
+            animationTime += Time.deltaTime;
+            t = animationTime / animationLength;
+        }
+        animationTime = 0f;
+        comboText.fontSize = maxFontSize;
+        comboText.color = new Color(originalTextColor.r, originalTextColor.g, originalTextColor.b, 255f);
     }
 
+    private IEnumerator ShrinkNumber()
+    {
+        var t = animationTime / animationLength;
+
+        while (t < 1)
+        {
+            // Return size.
+            var textFont = Mathf.Lerp(maxFontSize, minFontSize, t);
+            comboText.fontSize = textFont;
+
+            // Fade opacity.
+            var opacity = Mathf.Lerp(255f, originalTextColor.a, t);
+            comboText.color = new Color(originalTextColor.r, originalTextColor.g, originalTextColor.b, opacity);
+
+            animationTime += Time.deltaTime;
+            t = animationTime / animationLength;
+        }
+        animationTime = 0;
+        comboText.fontSize = minFontSize;
+        comboText.color = originalTextColor;
+
+        yield return null;
+    }
 }
